a1ab88ace9ef2c8d86faff58b97866b60afe0898
thiagodantasv <thiagodantasv@gmail.com>
2021-04-23 15:01:41 -0300

    create products component

---
+++ web/src/components/CreateProduct.tsx
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+
+function CreateProduct() {
+    return(
+        <div>products</div>
+    )
+}
+
+export default CreateProduct;
